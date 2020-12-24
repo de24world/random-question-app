72c6293f810139b9b900e99f282f6c792eff817a
HoSoo Lee <hosoo.lee@arithnea.de>
2020-12-24 21:21:43 +0100

    ReactCardFlip install & set

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from "react";
+import ReactCardFlip from "react-card-flip";
+
 import "./Question.css";
 import data from "../data/question.json";
 
 const Question = () => {
   const [questions, setQuestions] = useState("");
+  const [isFlipped, setIsFlipped] = useState(false);
+
   const getQuestion = () => {
     let randomNum = Math.floor(Math.random() * data.length);
     setQuestions(data[randomNum]);
+  };
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
   };
 
   useEffect(() => {
@@ -16,19 +24,30 @@ const Question = () => {
   console.log(data);
 
   return (
-    <div>
-      <div className="Question">
-        <div className="question" onClick={getQuestion}>
-          <p>{questions.text}?</p>
-          <p>Number: {questions.id}/100</p>
-        </div>
-        {/* <div className="btnContainer">
-          <button className="btn" onClick={getQuestion}>
-            Get Question
-          </button>
-        </div> */}
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+      <div>
+        This is the front of the card.
+        <button onClick={handleClick}>Click to flip</button>
       </div>
-    </div>
+
+      <div>
+        This is the back of the card.
+        <button onClick={handleClick}>Click to flip</button>
+      </div>
+      {/* 
+         <div className="Question">
+          <div className="question">
+            <p>{questions.text}?</p>
+            <p>Number: {questions.id}/100</p>
+          </div>
+          <div className="btnContainer">
+            <button className="btn" onClick={getQuestion}>
+              Get Question
+            </button>
+          </div>
+        </div>
+      */}
+    </ReactCardFlip>
   );
 };
 
