876fa15f5e5cef26a1e3fd1459f0c568be7a480f
HoSoo Lee <hosoo.lee@arithnea.de>
2020-12-26 22:51:22 +0100

    appheader add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./App.css";
 
+import AppHeader from "./components/AppHeader/AppHeader";
 import Card from "./components/Card/Card";
+
+import { Layout } from "antd";
+const { Header } = Layout;
 
 const App = () => {
   return (
-    <>
+    <Layout calssName="mainLayout">
+      <Header>
+        <AppHeader />
+      </Header>
       <Card />
-    </>
+    </Layout>
   );
 };
 
